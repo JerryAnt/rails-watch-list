be195e19c1a67d6decb9b347b92205df564b9bae
Jeremy Antoine <jeremincanada@gmail.com>
2021-10-05 09:00:39 -0400

    delete function lists#destroy added to lists#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   # A user can see the details of a given list and its name
   # A user can create a new list
   root to: "lists#index"
-  resources :lists, only: [:index, :show, :new, :create] do
-
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
   # A user can add a new bookmark (movie/list pair) to an existing list
   # -> so bookmark is nested in lists
 
@@ -17,7 +16,7 @@ Rails.application.routes.draw do
   # -> ...and bookmarks#create
   # A user can delete a bookmark from a list.
   # DELETE "bookmarks/25"
-  # -> so we have bookmarks#destroy
+   # -> so we have bookmarks#destroy
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: :destroy
